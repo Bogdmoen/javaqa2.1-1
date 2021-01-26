543f93b75020f910b4e7a80ebdf2306e8f13e1b2
Bogdan Klimchuk <bogd.moen@yandex.com>
2021-01-26 22:39:31 +0300

    code added

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket_cost = 9650_00;
+        int percent = 5;
+        int bonus_mile = ticket_cost * percent / 100 / 100;
+            System.out.println(bonus_mile);
+
+    }
+}
