a90963f2855ada3ce6e37e3895d897c84a210a15
Bogdan Klimchuk <bogd.moen@yandex.com>
2021-01-31 17:08:31 +0300

    var renamed in camelCase

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int ticket_cost = 9650_00;
+        int ticketCost = 9650_00;
         int percent = 5;
-        int bonus_mile = ticket_cost * percent / 100 / 100;
-            System.out.println(bonus_mile);
+        int bonusMile = ticketCost * percent / 100 / 100;
+        System.out.println(bonusMile);
 
     }
 }
